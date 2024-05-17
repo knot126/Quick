5807a151c98bf47d287077e071dc612f908c2d9c
knot126 <73200930+knot126@users.noreply.github.com>
2024-05-17 04:29:54 +0000

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Knot's Second Engine of All Time
+
+This is my second attempt to make a game eninge with a bit more planning this time, but hopefully not to the extent of suffering second system syndrome.
--- source/scene.h
+++ source/scene.h
@@ -4,9 +4,13 @@
  * In the engine, an entity is really just a script. Attributes are stored in
  * the script as script-local variables, and behaviour is implemented in the
  * script as functions.
+ * 
+ * XXX This isn't strictly true with the current plan for the engine. I plan to
+ * use a custom Smalltalk-derived language, which will basically use message
+ * passing between the engine and objects for everything.
  */
 typedef struct {
-	TrScript script;
+	TrScriptObject object;
 } TrEntity;
 
 /**
