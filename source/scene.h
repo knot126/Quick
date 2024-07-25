d4d9078ff78e4b92016ecf31d8b2c36f3f734f02
knot126 <73200930+knot126@users.noreply.github.com>
2024-07-25 16:21:38 +0000

    asdf

--- source/scene.h
+++ source/scene.h
@@ -1,23 +1,21 @@
 #pragma once
 
-/**
- * In the engine, an entity is really just a script. Attributes are stored in
- * the script as script-local variables, and behaviour is implemented in the
- * script as functions.
- * 
- * XXX This isn't strictly true with the current plan for the engine. I plan to
- * use a custom Smalltalk-derived language, which will basically use message
- * passing between the engine and objects for everything.
- */
-typedef struct {
-	TrScriptObject object;
-} TrEntity;
+#include "util/array.h"
+#include "util/table.h"
 
-/**
- * A scene contains a collection of entities. It should also probably contain
- * some kind of resource manager for *.script resources, honestly.
- */
 typedef struct {
-	TrEntity entities;
-	size_t entity_count;
-} TrScene;
+	/**
+	 * A context contains all of the loaded objects. It should also probably contain
+	 * some kind of resource manager for *.script resources, honestly.
+	 */
+	
+	DgArray objects;
+} TrContext;
+
+typedef struct {
+	/**
+	 * 
+	 */
+	
+	DgArray objects;
+} TrCollection;
